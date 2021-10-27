6ec5394fb5dc4e472a1e7b8304a24ca377709f98
Frank <fsolleveld@outlook.com>
2021-10-27 11:10:54 +0200

    Initial Commit

---
+++ NietAdelee/NietAdeleeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NietAdeleeApp.swift
+//  NietAdelee
+//
+//  Created by Frank Solleveld on 27/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct NietAdeleeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
